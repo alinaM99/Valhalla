3d334b58a68e6b60a14bed230ddbb6759bff3982
Alina <alinamtw99@gmail.com>
2023-11-19 10:48:59 +0530

    login page

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,14 +1,44 @@
 package com.example.notes
 
-import com.example.notes.R
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.notes.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setContentView(binding.root)
+        onClickListeners()
     }
 
+    private fun onClickListeners() {
+        binding.goBtn.setOnClickListener{
+            if (binding.etCode.text.toString().isEmpty()){
+              showAlertDialogg("Enter password to continue")
+            }
+            else if (!binding.etCode.text.toString().equals("2112")){
+                showAlertDialogg("Wrong password")
+            }
+            else{
+                showAlertDialogg("Correct password")
+            }
+        }
+    }
+    private fun showAlertDialogg(text: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(text)
+        builder.setPositiveButton("Ok") { dialogInterface, which ->
+            (dialogInterface as AlertDialog).dismiss()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
 }
